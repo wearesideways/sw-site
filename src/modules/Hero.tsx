f054eedbace3aa2a66f42d887d398b998e30429a
Mark Alfaro <mark.allan.alfaro@wearesideways.com>
2022-10-18 18:20:33 -0500

    Add hero module
    WIP.

--- src/modules/Hero.tsx
+++ src/modules/Hero.tsx
@@ -1,16 +1,18 @@
-export default function Hero({ src, alt }: any) {
+import styles from './Hero.module.scss'
+import Media from '../components/Media'
+
+type Props = {
+  slides?: any[]
+}
+
+export default function Hero({ slides }: Props) {
   return (
-    <>
-      <section className={'hero'}>
-        <div className={'hero-container'}>
-          <img src={src} alt={alt} />
-        </div>
-      </section>
-      <style jsx>{`
-        .hero {
-          display: flex;
-        }
-      `}</style>
-    </>
+    <section className={styles['hero']}>
+      <div className="swiper">
+        {slides?.map((slide, index) => (
+          <Media key={`slide-${index}`} {...slide.media} />
+        ))}
+      </div>
+    </section>
   )
 }
