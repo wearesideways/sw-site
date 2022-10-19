b1642457e6a509892d05c2454a4f957bf61cd75e
Mark Alfaro <mark.allan.alfaro@wearesideways.com>
2022-10-18 19:08:21 -0500

    Use swclasses from classic sites

--- src/components/Media.tsx
+++ src/components/Media.tsx
@@ -1,15 +1,30 @@
+import styles from './Media.module.scss'
+
 type Props = {
   src: string
   alt?: string
   type: string
   presentational?: boolean
+  classes?: string
 }
 
-export default function Media({ src, alt, type, presentational }: Props) {
+export default function Media({ src, alt, type, presentational, classes }: Props) {
   // TODO implement other media types
   if (type === 'img') {
+    const swmediaClasses = [
+      'swmedia',
+      styles['swmedia-img'],
+      styles['swmedia-lg'],
+      styles['swmedia-object'],
+    ]
+
     return (
-      <img src={src} alt={alt ?? ''} {...(presentational && { presentational: 'true', alt: '' })} />
+      <img
+        className={`${swmediaClasses.join(' ')} ${classes ?? ''}`}
+        src={src}
+        alt={alt ?? ''}
+        {...(presentational && { presentational: 'true', alt: '' })}
+      />
     )
   }
 
