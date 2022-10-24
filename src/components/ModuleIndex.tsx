2fbd93c429e73399ff7cf68622ffa75c13c10c99
Mark Alfaro <mark.allan.alfaro@wearesideways.com>
2022-10-24 18:53:19 -0500

    Register module to renderer

--- src/components/ModuleIndex.tsx
+++ src/components/ModuleIndex.tsx
@@ -1,5 +1,7 @@
 import HeadlineIntroModule from '../modules/HeadlineIntroModule'
+import HeroModule from '../modules/HeroModule'
 
 export const moduleIndex: { [key: string]: ({}: any) => {} } = {
   HeadlineIntroModule: HeadlineIntroModule,
+  HeroModule: HeroModule,
 }
