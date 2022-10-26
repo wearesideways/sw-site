dcd98d30b43e5e4de4b7fad7bc4840210943146f
Jaime Glez <jaime.gonzalez@sideways-nyc.com>
2022-10-26 17:50:04 -0500

    added module for headline no overflow with separator

--- src/components/ModuleIndex.tsx
+++ src/components/ModuleIndex.tsx
@@ -1,9 +1,11 @@
 import HeadlineIntroModule from '../modules/HeadlineIntroModule'
 import IntroColumnsModule from '../modules/IntroColumnsModule'
 import OverflowHeadlineModule from '../modules/OverflowHeadlineModule'
+import BigHeadlineModule from '../modules/BigHeadlineModule'
 
 export const moduleIndex: { [key: string]: ({}: any) => {} } = {
   HeadlineIntroModule,
   IntroColumnsModule,
   OverflowHeadlineModule,
+  BigHeadlineModule,
 }
---
+++ src/modules/BigHeadlineModule.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import styles from './BigHeadline.module.scss'
+import { Container } from 'react-bootstrap'
+
+type Props = {
+  headlineLeft: string
+  headlineRight: string
+}
+
+export default function OverflowHeadlineModule({
+  headlineLeft,
+  headlineRight,
+}: Props) {
+
+
+  return (
+    <section className={styles['root']}>
+      <Container>
+        <h2 className={styles['big-headline']}>
+          <span className={styles['headline-text']}>{headlineLeft}</span>
+          <span className={styles['line']}/>
+          <span className={styles['headline-text']}>{headlineRight}</span>
+        </h2>
+      </Container>
+    </section>
+  )
+}
