0daaf8c609b49e44a19f9462dbd3594d277a7792
Jaime Glez <jaime.gonzalez@sideways-nyc.com>
2022-10-31 12:57:28 -0600

    removed config

--- next.config.js
+++ next.config.js
@@ -4,17 +4,6 @@
  * @type {import('next').NextConfig}
  **/
 module.exports = {
-  // remove when we are able to upload real images
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'piscum.photos',
-        port: '',
-        pathname: '/id/**',
-      },
-    ],
-  },
   distDir: 'build',
   pageExtensions: ['tsx'],
   // we do linting and typecheking on CI, no need to do it on every CMS-issued build
