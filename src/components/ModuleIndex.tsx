ec59901a75d1d8b00334abc16a566ee9fe6d17e4
Mark Alfaro <mark.allan.alfaro@wearesideways.com>
2022-11-14 17:37:49 -0600

    Add module skeleton

--- src/components/ModuleIndex.tsx
+++ src/components/ModuleIndex.tsx
@@ -2,10 +2,12 @@ import HeadlineIntroModule from '../modules/HeadlineIntroModule'
 import IntroColumnsModule from '../modules/IntroColumnsModule'
 import OverflowHeadlineModule from '../modules/OverflowHeadlineModule'
 import BigHeadlineModule from '../modules/BigHeadlineModule'
+import ProjectsModule from '../modules/ProjectsModule'
 
 export const moduleIndex: { [key: string]: ({}: any) => {} } = {
   HeadlineIntroModule,
   IntroColumnsModule,
   OverflowHeadlineModule,
   BigHeadlineModule,
+  ProjectsModule,
 }
---
+++ src/modules/ProjectsModule.tsx
@@ -0,0 +1,5 @@
+import styles from './Projects.module.scss'
+
+export default function ProjectsModule() {
+  return <section className={styles['projects']}>projects-module</section>
+}
