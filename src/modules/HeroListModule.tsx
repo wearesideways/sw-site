a54ce7ff9ab3c6dc7ced121e76b7a7b22e168076
Jaime Glez <jaime.gonzalez@sideways-nyc.com>
2022-11-17 18:17:28 -0600

    attended PR comments

--- src/modules/HeroListModule.tsx
+++ src/modules/HeroListModule.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import styles from './HeroList.module.scss'
 import { Container, Row } from 'react-bootstrap'
 import { HeroItem, HeroItemProps } from '../components/HeroOverflowText'
+import classNames from 'classnames'
 
 type HeroProps = {
   heroItem: HeroItemProps
@@ -15,8 +16,8 @@ type Props = {
 export default function HeroListModule({ isContained, items }: Props) {
   return (
     <section className={styles['root']}>
-      <Container fluid={`${isContained ? 'md' : true}`}>
-        <Row className={`${!isContained ? styles['no-gutters'] : ''}`}>
+      <Container fluid={`${isContained ? 'xxl' : true}`} className={styles['list-container']}>
+        <Row className={classNames(!isContained && styles['no-gutters'], styles['list-row'])}>
           {items.map((i, idx) => {
             const isEvery3or4 = ((idx / 4) % 4) % 1 >= 0.5
             return <HeroItem key={idx} {...i.heroItem} isFullWidth={!isEvery3or4} />
