94ea65d4e062ff8a50a1efc709597da525be0960
Mark Alfaro <mark.allan.alfaro@wearesideways.com>
2022-11-24 13:48:01 -0600

    Implement desktop design

--- src/modules/ProjectsModule.tsx
+++ src/modules/ProjectsModule.tsx
@@ -1,5 +1,6 @@
 import styles from './Projects.module.scss'
 import { MediaTypes } from '../lib/types'
+import { Container, Row, Col } from 'react-bootstrap'
 import OverflownHeadline from '../components/OverflownHeadline'
 import Media from '../components/Media'
 
@@ -22,18 +23,32 @@ type Props = {
 export default function ProjectsModule({ projectsList }: Props) {
   return (
     <section className={styles['projects']}>
-      {projectsList?.map(({ project }, index) => {
-        return (
-          <figure className={styles['media-figure']} key={`project-fig-${index}`}>
-            <div className={styles['info-container']}>
-              {project.headline && <OverflownHeadline headline={project.headline} />}
-              <p className={styles['description']}>{project.description}</p>
-            </div>
+      <Container fluid={'md'} className={styles['projects-container']}>
+        <Row className={styles['projects-row']}>
+          {projectsList?.map(({ project }, index) => {
+            return (
+              <Col key={`project-fig-${index}`} md={6} lg={4}>
+                <figure className={styles['media-figure']}>
+                  <div className={styles['info-container']}>
+                    {project.headline && (
+                      <OverflownHeadline
+                        headline={project.headline}
+                        className={styles['overflown-headline']}
+                      />
+                    )}
+                    <h2 className={styles['headline']}>{project.headline}</h2>
+                    <p className={styles['description']}>{project.description}</p>
+                  </div>
 
-            <Media {...project.media} presentational={false} className={styles['media']} />
-          </figure>
-        )
-      })}
+                  <div className={styles['media-container']}>
+                    <Media {...project.media} presentational={false} className={styles['media']} />
+                  </div>
+                </figure>
+              </Col>
+            )
+          })}
+        </Row>
+      </Container>
     </section>
   )
 }
